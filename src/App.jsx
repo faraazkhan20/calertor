f1160a6663653abb103a629e656dd0d364a4e34f
Faraaz Khan <faraazkhan.me@gmail.com>
2025-08-15 21:04:42 +0530

    Added initial project files and setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1 style={{ marginBottom: "0px" }}>Calertor</h1>
+      <p style={{ marginTop: "0px" }}>Calertor - I built this to send me timely email alerts for birthdays, events, and important dates so I never miss anything important.</p>
+    </div>
+  );
+}
+
+export default App;
